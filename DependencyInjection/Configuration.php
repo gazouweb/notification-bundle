a6d980ad477a2cabab22276f9e59c23e6eb79b14
gazouweb <contact@gazouweb.com>
2021-03-18 12:09:02 +0100

    Update for SF4
    A tree builder without a root node is deprecated since Symfony 4.2 and will not be supported anymore in 5.0.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,10 +17,9 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('mgilet_notification');
+        $treeBuilder = new TreeBuilder('mgilet_notification');
 
-        $rootNode->children()
+        $treeBuilder->getRootNode()->children()
             ->scalarNode('notification_class')
                 ->cannotBeEmpty()
                 ->defaultValue(NotificationInterface::DEFAULT_NOTIFICATION_ENTITY_CLASS)
